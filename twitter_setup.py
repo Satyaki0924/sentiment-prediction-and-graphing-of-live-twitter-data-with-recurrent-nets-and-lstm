203dd7e81b507e0fec4b1898ec9b726ba82c3960
Satyaki0924 <sanyal.satyaki09@gmail.com>
2017-05-20 19:39:38 +0530

    fixing bugs

--- twitter_setup.py
+++ twitter_setup.py
@@ -7,10 +7,10 @@ class Setup(object):
     @staticmethod
     def get():
         # *** consumer key, consumer secret, access token, access secret. ***
-        c_key = "5FasqkkqsrwaLTKR4LQwjnuFT"
-        c_secret = "ppsjkcWvIF3nBDYlE6ZxMOQ6KwLJWtnka1Nr63yL1CxQ4q6ucS"
-        acc_token = "702550498524557312-MrIGfQy25GHQfmLwx6oNBK8pvt52JWN"
-        acc_secret = "L1OTnMPed3OJnkWfse9cepzVjBWg7Op50YVjBg3UeqtGh"
+        c_key = "ajksnadjksnfkjsdnjk3iep239ro24jiorj24oij4opi234j2r43rdf"
+        c_secret = "ajksnadjksnfkjsdnjk3iep239ro24jiorj24oij4opi234j2r43rdf"
+        acc_token = "ajksnadjksnfkjsdnjk3iep239ro24jiorj24oij4opi234j2r43rdf"
+        acc_secret = "ajksnadjksnfkjsdnjk3iep239ro24jiorj24oij4opi234j2r43rdf"
         return c_key, c_secret, acc_token, acc_secret
 
     @staticmethod
